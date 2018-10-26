57ce2506f363a3a14ede6e2dee9508ea94d913b6
keel-210 <crafters_touhou@yahoo.co.jp>
2018-10-26 17:56:52 +0900

    gakuenn

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityStandardAssets.Characters.ThirdPerson;
+
 
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
-[RequireComponent(typeof(ThirdPersonCharacter))]
+
 
 public class Enemy : MonoBehaviour {
 
     public GameObject target;
     NavMeshAgent agent;
-    ThirdPersonCharacter character;
+   
 
     // Use this for initialization
     void Start () {
 
         agent = GetComponent<NavMeshAgent>();
-        character = GetComponent<ThirdPersonCharacter>();
+        
 
     }
 	
